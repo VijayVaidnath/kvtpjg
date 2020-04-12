3240d0c1b8a4e5674709ee40a018dd1170c5f682
Vijay Vaidnath <vijaybhaskar.gv@gmail.com>
2020-04-12 09:19:18 -0500

    pom xml fixes

---
+++ rentalhub/rentalhub-web/src/main/java/com/kvtpj/rentalhub/web/RentalHubWeb.java
@@ -0,0 +1,17 @@
+package com.kvtpj.rentalhub.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vijay Vaidnath
+ *
+ */
+@SpringBootApplication
+public class RentalHubWeb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalHubWeb.class, args);
+	}
+
+}
